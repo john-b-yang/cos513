d20599cfbd275a96441a2fcafda5cb3753872f1b
John Yang <john.yang20@berkeley.edu>
2022-03-29 14:39:42 -0400

    Adaptive Bloom Filter impl

--- journal.md
+++ journal.md
@@ -1,13 +1,22 @@
 # Journal
 
-**March 23, 2021**
+**March 29, 2022**
+Finished adaptive bloom filter implementation. This approach is described in the Ada-BF [paper](https://openreview.net/pdf?id=rJlNKCNtPB) published in ICLR 2020. It builds upon the learned bloom filter approach. With LBF, we divided the data `X` into two groups that are evaluated as follows (for an individual data sample):
+* If score(x) >= threshold, `x` is purported to exist (0 hash functions used)
+* If score(x) < threshold, `x`'s membership is evaluated with the bloom filter (K hash functions used)
+
+The improvement by the adaptive bloom filter is that instead of just these 2 groups, `X` is divided into *g* groups by score, where each group uses a different number of hash functions is used to test membership. The threshold ranges that each group cover do not overlap, and by segmenting the data with finer granularity, this newfound flexibility allows the data structure to fine-tune the FPR across smaller regions, which empirically and formally leads to a lower false positive rate. In this sense, Ada-BF is a generalization of the learned bloom filter, where there are only two groups separated by a single threshold, and the hash functions per group are `K` and 0 respectively. Given the same amount of memory, Ada-BF is able to use the storage space with a much greater degree of granularity, leading to much stronger performance that is also more reflective of real world data.
+
+At this point, I think the next two bloom filters I tackle will be the disjoint adaptive bloom filter along with the sandwiched bloom filter, which I hope to be able to complete within the next week or two. After that, I want to begin to put together the final project report that will be a comparison of these different bloom filters, proving how the design choices each one makes translates into better performance and more efficient storage space. Time permitting, I think it'd be fun to explore more learned index structures (I found a paper on learned KNN indexing that was an interesting adoption of learning techniques to data structures. I'll try to find the paper for that and implement it). This week during lecture, we discussed Bayesian neural networks, and the textbook also discussed how the properties of Bayes' Rules and their re-expression of priors or posterior + likelihoods in learning algorithms can bolster performance. I think it'd be worthwhile to learn how to implement the Bayesian version of a traditional learning algorithm, such as Bayesian Principle Component Analysis.
+
+**March 23, 2022**
 Finished learned bloom filter implementation. The code was surprisingly not as complex or efficient as I thought it was going to be. The implementaion is based on *The Case for Learned Index Structures (Kraska 2018)* paper, which reframes the problem that index structures try to solve as a learning problem; specifically, the authors discuss how creating such a data structure is quite similar to learning a cumulative distribution function. Once the nature of the dataset's distribution is known, optimizations can be made accordingly.
 
 The learned bloom filter feels like a pretty naive attempt at this. Based on the confidence of a classifier regarding whether a value exists, if the confidence is high enough, then the classifier's answer is used. Otherwise, the model falls back on a vanilla bloom filter. Since the amount of data that the bloom filter would be used for, it can perform effectively for values that the classifier had a harder time discerning.
 
 Instead of re-implementing, which was my initial approach, I just used the naive bloom filter again, and the `LearnedFilter` class mainly contains the iterative logic that tests for the best threshold in increments of 0.01. Although it works correctly and adheres to the description of the original implementation, it is by no means a very complex or intelligent model. Next up is the adaptive bloom filter, which I believe incorporates more learning and should be quite interesting to tackle.
 
-**March 17, 2021**
+**March 17, 2022**
 Began on project. Since the abstract, I've pivoted from reinforcement learning to bloom filters. While I'd like to explore RL at some point, the application of such techniques to games feels like a solved problem. I recently read the [OnRL](https://ml-video-seminar.princeton.systems/papers/onRL.pdf) paper, which got me thinking about RL as a tool for creating learned indices and data structures, which I found interesting.
 
 My end goal would be to find some novelty within the intersection of RL and systems functions. Traditional data structures involve incorporating general heuristics into their design at the cost of expensive edge cases; recent supervised and machine learning approaches have demonstrated significant gains due to their ability to learn the dataset distribution and optimize towards it.
---
+++ src/bloom_filters/adaptive.py
@@ -0,0 +1,142 @@
+import argparse
+import numpy as np
+import pandas as pd
+from naive import hashfunc
+
+DEBUG = True
+
+# Argparse Definitions
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', action='store', dest="data_path", type=str, required=True, help="path of the dataset")
+parser.add_argument('--size_of_BF', action='store', dest="hash_len", type=int, required=True, help="size of the bloom filter")
+parser.add_argument('--c_min', action="store", dest="c_min", type=float, required=True, help="minimum ratio of the keys")
+parser.add_argument('--c_max', action="store", dest="c_max", type=float, required=True, help="maximum ratio of the keys")
+parser.add_argument('--group_min', action="store", dest="group_min", type=int, required=True, help="minimum number of groups")
+parser.add_argument('--group_max', action="store", dest="group_max", type=int, required=True, help="maximum number of groups")
+
+class AdaptiveFilter():
+  def __init__(self, n: int, hash_len: int, k: int):
+    self.n, self.hash_len = n, hash_len
+    self.h = []
+    for _ in range(k):
+      self.h.append(hashfunc(self.hash_len))
+    self.table = np.zeros(self.hash_len, dtype=int)
+  
+  def insert(self, key, k):
+    for i in range(int(k)):
+      t = self.h[i](key)
+      self.table[t] = 1
+  
+  def test(self, key, k):
+    test_result, match = 0, 0
+    for j in range(int(k)):
+      t = self.h[j](key)
+      match += 1*(self.table[t] == 1)
+    if match == k:
+      test_result = 1
+    return test_result
+
+def SearchBestFilter(c_min, c_max, group_min, group_max, hash_len, train_negative, positive_samples):
+  k_min, c_opt = 0, 0
+  FP_opt = train_negative.shape[0]
+
+  # Iterate across number of groups (to split the dataset into, i.e. number of bloom filters)
+  for k_max in range(group_min, group_max+1):
+    # Iterate across the possible thresholds
+    for c in np.arange(c_min, c_max+10**(-3), 0.1):
+      tau = sum(c ** np.arange(0, k_max - k_min + 1, 1))
+
+      # Create bloom filter for this bucket
+      n = positive_samples.shape[0]
+      bloom_filter = AdaptiveFilter(n, hash_len, k_max)
+      
+      # Determine thresholds based on number of groups being used
+      thresholds = np.zeros(k_max - k_min + 1)
+      thresholds[-1] = 1.1
+
+      # Determine amount of training data that falls in the bucket
+      num_negative = sum(train_negative['score'] <= thresholds[-1])
+      num_piece = int(num_negative / tau) + 1
+
+      # Get list of scores
+      score = train_negative.loc[(train_negative['score'] <= thresholds[-1]), 'score']
+      score = np.sort(score)
+
+      # Iterate across number of groups, adjust thresholds based on proximity of scores
+      for k in range(k_min, k_max):
+        i = k - k_min
+        score_1 = score[score < thresholds[-(i+ 1)]]
+        if int(num_piece * c ** i) < len(score_1):
+          thresholds[-(i+2)] = score_1[-int(num_piece * c ** i)]
+      
+      url, score = positive_samples['url'], positive_samples['score']
+
+      for score_s, url_s in zip(score, url):
+        ix = min(np.where(score_s < thresholds)[0])
+        k = k_max - ix
+        bloom_filter.insert(url_s, k)
+      
+      # Test the configured bloom filter
+      ML_positive = train_negative.loc[(train_negative['score'] >= thresholds[-2]), 'url']
+      temp = (train_negative['score'] < thresholds[-2])
+      url_negative, score_negative = train_negative.loc[(temp, 'url')], train_negative.loc[(temp, 'score')]
+      
+      test_result = np.zeros(len(url_negative))
+      ss = 0
+      for score_s, url_s in zip(score_negative, url_negative):
+        ix = min(np.where(score_s < thresholds)[0])
+        k = k_max - ix
+        test_result[ss] = bloom_filter.test(url_s, k)
+        ss += 1
+      
+      # Calculate # of FP items, FP rate. Keep if best performing bloom filter
+      FP_items = sum(test_result) + len(ML_positive)
+
+      if FP_opt > FP_items:
+        FP_opt = FP_items
+        bloom_filter_opt = bloom_filter
+        thresholds_opt = thresholds
+        k_max_opt = k_max
+        c_opt = c
+  
+  if DEBUG:
+    print('Optimal FPs: %f, Optimal c: %f, Optimal num_group: %d' % (FP_opt, c_opt, k_max_opt))
+
+  return bloom_filter_opt, thresholds_opt, k_max_opt
+
+if __name__ == '__main__':
+  results = parser.parse_args()
+  DATA_PATH, hash_len = results.data_path, results.hash_len
+  c_min, c_max, group_min, group_max = results.c_min, results.c_max, results.group_min, results.group_max
+
+  data = pd.read_csv(DATA_PATH)
+
+  # Grab negative, positive samples from dataset
+  negative_samples = data.loc[(data['label'] == -1)]
+  positive_samples = data.loc[(data['label'] == 1)]
+
+  # Get negative training data sample to test different thresholds
+  train_negative = negative_samples.sample(frac=0.3)
+
+  # Search for optimal bloom filter
+  bloom_filter, thresholds, k_max = SearchBestFilter(c_min, c_max, group_min, group_max, hash_len, train_negative, positive_samples)
+
+  # Test Queries
+  ML_positive = negative_samples.loc[(negative_samples['score'] >= thresholds[-2]), 'url']
+  query_negative = negative_samples.loc[(negative_samples['score'] < thresholds[-2]), 'url']
+  score_negative = negative_samples.loc[(negative_samples['score'] < thresholds[-2]), 'score']
+
+  # Evaluate on test dataset
+  test_result = np.zeros(len(query_negative))
+  ss = 0
+
+  for score_s, query_s in zip(score_negative, query_negative):
+    ix = min(np.where(score_s < thresholds)[0])
+    k = k_max - ix
+    test_result[ss] = bloom_filter.test(query_s, k)
+    ss += 1
+  
+  FP_items = sum(test_result) + len(ML_positive)
+  FPR = FP_items / negative_samples.shape[0]
+
+  print("False positive items: {}; FPR: {}; Size of queries: {}".format(FP_items, FPR, negative_samples.shape[0]))
